120f39091df001fc770aeea35f656f8efc211b54
monsoorch <monsoor.zontro@gmail.com>
2021-05-18 02:49:45 +0530

    ascii values

---
+++ data_structure_and_algo/src/dsaproblems/Ascii.java
@@ -0,0 +1,11 @@
+package dsaproblems;
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<256;i++) {
+			System.out.println("Ascii value of : "+(char)i+ " is : "+i);
+		}
+	}
+}
