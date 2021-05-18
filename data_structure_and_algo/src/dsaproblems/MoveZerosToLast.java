03a07e8cc4cf9cc9cade0f398ac3c8f7abe05b4c
monsoorch <monsoor.zontro@gmail.com>
2021-05-18 19:45:38 +0530

    added new prog

---
+++ data_structure_and_algo/src/dsaproblems/MoveZerosToLast.java
@@ -0,0 +1,20 @@
+package dsaproblems;
+
+import java.util.Arrays;
+
+public class MoveZerosToLast {
+	public static void main(String args[]) {
+		int arr[]= {0,1,5,4,0,6,0};
+		int j=0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=0) {
+				arr[j++]=arr[i];
+			}
+		}
+		while(j<arr.length) {
+			arr[j++]=0;
+		}
+		System.out.println(Arrays.toString(arr));
+	}		
+}
+
